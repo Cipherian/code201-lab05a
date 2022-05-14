1958202757bfe0259dc7490343c9ee6c62670495
Daniel Brott <danbrott42@gmail.com>
2022-05-14 15:40:22 -0700

    updating p3

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
+function sum(a, b, c) {
   //eslint-disable-line
-  return [a + b, `The sum of ${a} and ${b} is ${a + b}.`];
+  a + b + c;
+  return a + b + c;
+  // return [a + b, `The sum of ${a} and ${b} is ${a + b}.`];
   //This one was already fixed when I downloaded it, just adding a comment to verify that I looked at it.
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -29,13 +31,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 
-function multiply(a, b) {
-  return [a * b, 'The product of 5 and 9 is 45.'];
-  //eslint-disable-line
+function multiply(a, b, c) {
+  a * b * c;
+  return a * b * c;
+  // return [a * b, "The product of 5 and 9 is 45."];
+  //eslint-disable-line 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5, 9);
+// testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,11 +56,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+  return [
+    sum(a, b, c),
+    multiply(a, b, c),
+    '4 and 7 and 5 sum to 16.',
+    'The product of 4 and 7 and 5 is 140.',
+  ];
   //eslint-disable-line
+  console.log(sum(a,b,c));
+  console.log(multiply(a,b,c));
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
